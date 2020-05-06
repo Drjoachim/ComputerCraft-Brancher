465dbc218febdf68a0d4064b23d6c831d42af387
Joachim De Roissart <Joachim.DeRoissart@codit.eu>
2020-05-06 16:02:49 +0200

    calculation added and refueling

--- Brancher.lua
+++ Brancher.lua
@@ -2,7 +2,7 @@
 
 -- 06/05/2020
 -- DrJoachim
-
+local version = '0.05'
 
 -- ToDoList
 -- Refactor code
@@ -15,20 +15,26 @@
 -- Mabye add code that make turtle make new line of tunnels
 
 --Local
-local distance = 0 -- How Far Did User Pick
-local onlight = 0 -- When to Place Torch
+local branchDepth = 0 -- How deep Did User Pick
+local branchNumber = 0 -- How many branches does the user want?
 
 local torchCount = 0 -- Tracks the number of torches
+local torchLocations = {}
+
 local chestCount = 0 -- Tracks the number of chests
+local chestLocations = {}
+
 local fuelCount = 0 -- Tracks the number of fuelItems
+local fuelLocations = {}
 
 local MD = 3 -- How Many Blocks Apart From Each Mine
-local MineTimes = 0 -- If Multi Mines Are ON then This will keep Count
+
+local onlight = 0 -- When to Place Torch
 local Fuel = 0 -- if 2 then it is unlimited no fuel needed
 local NeedFuel = 0 -- If Fuel Need Then 1 if not Then 0
 local Error = 0 -- 0 = No Error and 1 = Error
 local Way = 0 -- 0 = Left and 1 = Right
-local version = '0.04'
+
 
 local trash = {
 	["minecraft:cobblestone"] = true,
@@ -42,63 +48,50 @@ local function initializeLocations()
 		turtle.select(i)
 		local details = turtle.getItemDetail(i);
 		if details ~= nil then
-			if string.find(details.name,"torch") then
-				-- Found torch location
-				logger.log("Found torch location "..i)
-				 torchCount = torchCount + turtle.getItemCount(i)
-			elseif string.find(details.name,"chest") then
-				logger.log("Found chest location "..i)
+			if string.find(details.name,"torch") then						
+				torchCount = torchCount + turtle.getItemCount(i)
+				table.insert(torchLocations,i)
+			elseif string.find(details.name,"chest") then			
 				chestCount = chestCount + turtle.getItemCount(i)
+				table.insert(chestLocations,i)
 			elseif turtle.refuel(0) then
-				logger.log("Found fuel location "..i)
 				fuelCount = fuelCount + turtle.getItemCount(i)
+				table.insert(fuelLocations,i)
 			end
 		end
 	end
 	turtle.select(1)
-	logger.log("Found "..torchCount.." torches.")
-	logger.log("Found "..chestCount.." chests.")
-	logger.log("Found "..fuelCount.." fuel items.")
+	logger.log("*********************Locations****************************")
+	logger.log("Found "..torchCount.." torches on these locations: "..torchLocations)
+	logger.log("Found "..chestCount.." chests on these locations: "..chestLocations)
+	logger.log("Found "..fuelCount.." fuel items on these locations: "..fuelLocations)
+	logger.log("*******************************************************")
 end
 
 --Checking
 local function Check()
-
-	
-	
-
-
-	if torch == 0 then
-		print("There are no torch's in Turtle")
-		Error = 1
-	else
-		print("There are torch's in turtle")
-	end
-	if chest == 0 then
-		print("there are no chests")
-		Error = 1
-	else
-		print("There are chest in turtle")
-	end
-	if ItemFuel == 0 then
-		print("No Fuel Items")
-		Error = 1
-	else
-		print("there is fuel")
-	end
-	repeat
-		if turtle.getFuelLevel() == "unlimited" then 
-			print("NO NEED FOR FUEL")
-			Needfuel = 0
-		elseif turtle.getFuelLevel() < 100 then
-			turtle.select(3)
-			turtle.refuel(1)
-			Needfuel = 1
-			ItemFuel = ItemFuel - 1
-		elseif NeedFuel == 1 then
-			Needfuel = 0
+	local branchSteps = branchDepth * 2 * branchNumber
+	local mainSteps = 9 * branchNumber
+	local totalSteps = branchSteps + mainSteps
+	logger.log("totalsteps = "..totalSteps)
+	if turtle.getFuelLevel() == "unlimited" then 
+		logger.log("No need for fuel")
+		
+	elseif  turtle.getFuelLevel() >= totalSteps then
+		logger.log("There should be enough fuel to mine the whole branch")
+	elseif turtle.getFuelLevel() < totalSteps then
+		logger.log("No fuel enough, refueling")
+		local x = 1
+		while turtle.getFuelLevel() < totalSteps or x<=#fuelLocations do
+			local v = fuelLocations[x]
+			logger.log("refueling with location "..x)
+			turtle.select(v)
+			turtle.refuel(turtle.getItemCount(v))
+			x = x + 1
+			logger.log("fuel level is now "..turtle.getFuelLevel)
 		end
-	until NeedFuel == 0
+	end
+
 end
 
 -- Recheck if user forget something turtle will check after 15 sec
@@ -253,18 +246,19 @@ function initializeLogger()
 end
 
 -- Start
-print("Hi There Welcome to Mining Turtle Program "..version)
-print("How Far Will Turtle Go")
+print("Hi There Welcome to Mining Turtle Program v"..version)
+print("How deep should the branch mines be?")
 input = io.read()
-distance = tonumber(input)
-TF = distance
-TB = distance
+branchDepth = tonumber(input)
+
+print("How many branches do you want?")
+input = io.read()
+branchNumber = tonumber(input)
+
 initializeLogger()
 initializeLocations()
--- print("How Many Times")
--- input3 = io.read()
--- MineTimes = tonumber(input3)
--- Check()
+
+Check()
 -- if Error == 1 then 
 -- 	repeat
 -- 		sleep(10)
